2ccb9c4160b78e4cfdfa9f72af70b8ef7af01fe3
jaseena-k <jaseenajas596@gmail.com>
2026-04-03 12:32:45 +0530

    signing page done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,24 @@
 import express from "express"
-import { ENV } from "./config/env.js"
+import { ENV } from "../config/env.js"
+import { connectDB } from "../config/db.js"
+import { clerkMiddleware } from '@clerk/express'
+import { inngest, functions } from "./src/inngest"
+
 
 const app = express()
+app.use(clerkMiddleware())
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get("/",(req,res)=>{
     res.send("Hello World!!")
     console.log("mongoose url",ENV.DB_URL)
 
 })
 
-app.listen(ENV.PORT,()=> console.log ("server started on port",ENV.PORT))
+app.listen(ENV.PORT,()=>{
+    console.log ("server started on port",ENV.PORT)
+    connectDB();
+})
 
 
 // ctHoeUXzDXo7AqSx
