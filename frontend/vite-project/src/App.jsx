4c73516fb35b9adaf32fb6799e21c4baf5338cb2
jaseena-k <jaseenajas596@gmail.com>
2026-04-10 15:44:09 +0530

    Authpage Impliment

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import {SignedIn,SignedOut,SignInButton,SignUpButton,UserButton} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
+import AuthPage from './pages/AuthPage.jsx';
+import { Routes, Route, Navigate } from 'react-router';
+import HomePage from './pages/HomePage.jsx';
 
 function App() {
   return (
     <div>
-        <header>
-        <SignedOut>
-          <SignInButton mode='model'/>
-          <SignUpButton />
-        </SignedOut>
+      <>
         <SignedIn>
-          <UserButton />
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/auth" element={<navigate to={"/"} replace />}></Route>
+          </Routes>
         </SignedIn>
-      </header>
+        <SignedOut>
+          <Routes>
+            <Route path="/auth" element={<AuthPage />}></Route>
+            <Route path="*" element={<Navigate to={"/auth"} replace />}></Route>
+          </Routes>
+        </SignedOut>
+
+      </>
 
     </div>
   )
